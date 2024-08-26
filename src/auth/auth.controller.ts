6563fd281f5c6ad43a1699cbfccc97bd9f1fffc6
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-08-26 17:10:37 -0300

    auth.controller.ts

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { User } from '../users/entities/user.entity';
+import { Public } from '../decorators/public.decorator';
+import { Role, Roles } from '../decorators/roles.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Roles(Role.Admin)
+  @Post('register')
+  async register(
+    @Res() response: Response,
+    @Body() createUserDto: CreateUserDto,
+  ) {
+    const createdUser = await this.authService.register(createUserDto);
+    return response.status(HttpStatus.CREATED).json(createdUser);
+  }
+
+  @Public()
+  @Post('login')
+  async signIn(
+    @Res() response: Response,
+    @Body() user: Pick<User, 'email' | 'password'>,
+  ) {
+    const successResp = await this.authService.signIn(user);
+    return response.status(HttpStatus.ACCEPTED).json(successResp);
+  }
+}
