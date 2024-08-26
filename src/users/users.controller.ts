c3d44a0dc034f1f99a710e3d824b44b87d10e757
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-08-26 16:54:00 -0300

    feature: users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get, HttpStatus,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Response, response } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(): Promise<Response<User[]>> {
+    const users = this.usersService.findAll();
+    return response.status(HttpStatus.ACCEPTED).json(users);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOneById(@Param('id') id: string): Promise<Response<User>> {
+    const user = await this.usersService.findOneById(+id);
+    return response.status(HttpStatus.ACCEPTED).json(user)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto
+  ): Promise<Response> {
+    await this.usersService.update(+id, updateUserDto);
+    return response.status(HttpStatus.ACCEPTED);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
 }
